02dfcffe9460e6787da4a3df5cdb0f766672d8b8
djordje <djordje.stefanovic89@gmail.com>
2023-02-21 16:54:37 +0100

    better structure

--- languages.js
+++ languages.js
@@ -1,14 +1,17 @@
 const jsonEN = {
   storage_sub: ' We offer what everyone seems to be missing — space.',
-  storage_tit:' Rent storage units for your personal possessions.'
+  storage_tit:' Rent storage units for your personal possessions.',
+  storage_link:'Storage'
 };
 
 const jsonSR = {
   storage_sub: 'Nudimo ono što izgleda svima nedostaje - prostor',
-  storage_tit:'Iznajmite skladišne jedinice za svoje lične potrebe.'
+  storage_tit:'Iznajmite skladišne jedinice za svoje lične potrebe.',
+  storage_link:'Skaldište'
 };
 
 $('.en').click(function () {
+    document.documentElement.setAttribute("lang", 'en');
     $('.en').removeClass('active')
     $('.sr').addClass('active')
   for (let key in jsonEN) {
@@ -17,9 +20,28 @@ $('.en').click(function () {
 });
 
 $('.sr').click(function () {
+    document.documentElement.setAttribute("lang", 'sr');
     $('.sr').removeClass('active')
     $('.en').addClass('active')
   for (let key in jsonSR) {
     document.querySelector('.' + key).textContent = jsonSR[key];
   }
 });
+
+// window.onload = function () {
+//     for (let key in jsonSR) {
+//         document.querySelector('.' + key).textContent = jsonSR[key];
+//       }
+// }
+
+
+
+
+// fetch('https://reqbin.com/echo/get/json', {
+//     method: 'GET',
+//     headers: {
+//         'Accept': 'application/json',
+//     },
+// })
+//    .then(response => response.json())
+//    .then(response => console.log(JSON.stringify(response)))
