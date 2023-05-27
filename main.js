ba94e17b9bdbd1c05a4ecaea2fbd1f752bc4c5e6
djordje <djordje.stefanovic89@gmail.com>
2023-05-27 12:52:47 +0200

    changes

--- main.js
+++ main.js
@@ -424,7 +424,7 @@ window.addEventListener('load', function (event) {
     el: document.querySelector('[data-scroll-container]'),
     smooth: true,
     getSpeed: true,
-    smoothMobile: true,
+    // smoothMobile: true,
     // smartphone: { smooth: true },
     getDirection: true,
     reloadOnContextChange: true,
